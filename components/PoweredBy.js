6e74c498936d7fd55c250ff4635b06674b7573a7
Man-Cur <mangostorecanada@gmail.com>
2025-07-03 19:21:59 -0400

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,7 +9,7 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://mangocuration.com'
         className='underline justify-start'>
         MangoCuration @2025
       </a>
